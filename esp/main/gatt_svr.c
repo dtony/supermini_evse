da5d32be6c2e9b2018d0dc8225acb9ce3fe19a95
Tony Ducrocq <tony.ducrocq@gmail.com>
2026-06-17 08:21:49 +0200

    feat(ble): add service change in case of new characteristic

--- esp/main/gatt_svr.c
+++ esp/main/gatt_svr.c
@@ -8,6 +8,7 @@ uint16_t ota_control_val_handle;
 uint16_t ota_data_val_handle;
 uint16_t current_amp_val_handle;
 uint16_t pwm_cal_val_handle;
+uint16_t service_changed_val_handle;
 
 const esp_partition_t *update_partition;
 esp_ota_handle_t update_handle;
@@ -45,6 +46,11 @@ static int gatt_svr_chr_pwm_cal_cb(uint16_t conn_handle,
                                    uint16_t attr_handle,
                                    struct ble_gatt_access_ctxt *ctxt,
                                    void *arg);
+
+static int gatt_svr_chr_service_changed_cb(uint16_t conn_handle,
+                                           uint16_t attr_handle,
+                                           struct ble_gatt_access_ctxt *ctxt,
+                                           void *arg);
 
 static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
     {// Service: Device Information
@@ -130,9 +136,36 @@ static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
     },
 
     {
+        // Service: Generic Attribute (for Service Changed)
+        .type = BLE_GATT_SVC_TYPE_PRIMARY,
+        .uuid = BLE_UUID16_DECLARE(0x1800), // Generic Attribute Service
+        .characteristics =
+            (struct ble_gatt_chr_def[]){
+                {
+                    // Characteristic: Service Changed
+                    .uuid = BLE_UUID16_DECLARE(GATT_SERVICE_CHANGED_UUID),
+                    .access_cb = gatt_svr_chr_service_changed_cb,
+                    .flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_NOTIFY,
+                    .val_handle = &service_changed_val_handle,
+                },
+                {
+                    0,
+                }},
+    },
+
+    {
         0,
     },
 };
+
+static int gatt_svr_chr_service_changed_cb(uint16_t conn_handle,
+                                          uint16_t attr_handle,
+                                          struct ble_gatt_access_ctxt *ctxt,
+                                          void *arg) {
+  uint8_t val = 0;
+  int rc = os_mbuf_append(ctxt->om, &val, sizeof(val));
+  return rc == 0 ? 0 : BLE_ATT_ERR_INSUFFICIENT_RES;
+}
 
 static int gatt_svr_chr_access_device_info(uint16_t conn_handle,
                                            uint16_t attr_handle,
